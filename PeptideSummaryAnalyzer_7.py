9a7e1ebea724a923b8f21723c59da25240df9cd5
Ae-Mc <ae_mc@mail.ru>
2020-01-22 19:08:13 +0300

    First commit

---
+++ PeptideSummaryAnalyzer_7.py
@@ -0,0 +1,313 @@
+from os import listdir, mkdir
+from os.path import exists
+from math import floor
+
+INPUTPATH = "Input"
+OUTPUTPATH = "Output/"
+
+# Номера столбцов, в которых содержаться нужные данные
+positions = {
+	"Unused": 1,
+	"Id": 6,
+	"Contribution": 10,
+	"Confidence": 11,
+	"Sequence": 12,
+	"Sc": 21,
+	"Precursor Signal": 24
+}
+# Список параметров, для которых создаются выходные файлы
+params = ["Unused", "seq_length_summ", "counts", "Sc_summ", "Psignal_summ", "Sc_norm", "Psignal_norm", "SP_2"]
+# Список параметров, присутствующих в output.txt
+outParams = ["Unused", "seq_length_summ", "counts", "Sc_summ", "Psignal_summ", "Sc_norm", "Psignal_norm", "SP_2", "seq_length"]
+
+# Класс, реализующий параметры базового id
+class ID(dict):
+	def __init__(self):
+		for param in outParams:
+			self[param] = 0
+
+# Ввод начальных данных
+def inputParams(idFileName, idExclusionFileName, dbFileName, input_unused,
+	input_contribution, input_confidence, input_minGroupsWithId, input_maxGroupAbsence):
+	# Функция, получающая из строки метод сравнения и числовое значение
+	def getOpData(string):
+		op = ""
+		for ch in string:
+			if ch in "!<>=":
+				op += ch
+			else:
+				break
+		if op == "":
+			return "", None
+		return op, float(string[len(op):])
+
+	IDs = []
+	if idFileName != "":
+		tempFile = open(idFileName)
+		IDs = tempFile.read().split("\n")
+		tempFile.close()
+
+	IDExclusionList = []
+	if idExclusionFileName != "":
+		tempFile = open(idExclusionFileName)
+		IDExclusionList = tempFile.read().split("\n")
+		tempFile.close()
+
+	if dbFileName != "":
+		tempFile = open(dbFileName)
+		strings = tempFile.read().split("\n")
+		tempFile.close()
+		seqDB = {}  # Хранит длины последовательностей для каждого id
+		seqDescDB = {}  # Хранит описания для каждого id
+		i = 0
+		while (i < len(strings)):
+			if len(strings[i]):
+				if strings[i][0] == '>':
+					seqID = strings[i].split(' ')[0][1:]
+					seqDB[seqID] = 0
+					if len(strings[i].split(" ")) > 1:
+						seqDescDB[seqID] = strings[i].split(" ")[1]
+						for word in strings[i].split(" ")[2:]:
+							if word.startswith("OS="):
+								break
+							seqDescDB[seqID] += " " + word
+					else:
+						seqDescDB[seqID] = ""
+					i += 1
+					while ( (i < len(strings)) and
+									( (not len(strings[i])) or strings[i][0] != '>')
+								):
+						seqDB[seqID] += len([ch for ch in strings[i] if ch in "ABCDEFGHIKLMNOPQRSTUVWXYZ"])
+						i += 1
+					i -= 1
+			i += 1
+
+		for i in seqDB:
+			if seqDB[i] == 0:
+				input("!!!ERROR!!!\nLength of sequence with id " + i + " = 0")
+				raise()
+
+		unusedOp, unused = getOpData(input_unused)
+		contribOp, contrib = getOpData(input_contribution)
+		confOp, conf = getOpData(input_confidence)
+		minGroupsWithId = int(input_minGroupsWithId)
+		maxGroupAbsence = input_maxGroupAbsence
+		return idFileName, IDs, IDExclusionList, seqDB, seqDescDB, unusedOp, unused, contribOp, contrib, confOp, conf, minGroupsWithId, maxGroupAbsence
+	else:
+		input("!!!ERROR!!!\nNo database file!")
+		raise()
+
+
+# Считывание и занесение в оперативную память информации из файлов из папки INPUTPATH
+def readInputFiles():
+	fileNames = []
+	for fileName in listdir(path=INPUTPATH):
+		fileNames.append(fileName)
+	db = {}
+	# Сортировка массива с именами файлов
+	newArr = [float(val.split("_")[0]) for val in fileNames]
+	for i in range(0, len(newArr)):
+		for j in range(i, len(newArr)):
+			if newArr[i] > newArr[j]:
+				newArr[i] += newArr[j]
+				newArr[j] = newArr[i] - newArr[j]
+				newArr[i] -= newArr[j]
+				newArr[i] = round(newArr[i], 5)
+				newArr[j] = round(newArr[j], 5)
+
+	shortFileNames = [str(val) for val in newArr]
+
+	for name in fileNames:
+		tempFile = open(INPUTPATH + "/" + name)
+		tempArray = []
+		for string in tempFile.read().split("\n")[1:]:
+			tempArray.append(string.split("\t"))
+		if tempArray[-1] == ['']:
+			tempArray.pop()
+		db[name.split("_")[0]] = tempArray
+		del(tempArray)
+		tempFile.close()
+
+	rawOutput = {}
+	for fileName in shortFileNames:
+		if fileName != "":
+			rawOutput[fileName] = {}
+
+	return fileNames, db, shortFileNames, rawOutput
+
+# Структурирование данных из файлов, загруженных в оперативную память. Первичный параметр - имя файла
+def getInfoFromFiles(idFileName, IDs, IDExclusionList, seqDB, db, rawOutput, confOp, conf, contribOp, contrib, unusedOp, unused):
+	# Сравнивает arg1 и arg2 с помощью операции, указанной в op
+	def compare(op, arg1, arg2):
+		if op == "":
+			return True
+		if op == "=":
+			return arg1 == arg2
+		return(eval(str(arg1) + op + str(arg2)))
+
+	for fileName in db:
+		rawOutput[fileName]["Sc_file_summ"] = 0
+		rawOutput[fileName]["Psignal_file_summ"] = 0
+
+		for line in db[fileName]:
+			if line == "":
+				continue
+			lineId = line[positions["Id"]].split("; ")[0]
+			if (compare(confOp, float(line[positions["Confidence"]]), conf) and
+				compare(contribOp, float(line[positions["Contribution"]]), contrib) and
+				compare(unusedOp, float(line[positions["Unused"]]), unused)):
+				# Если ID находится в белом списке, или если файл с белым списком не был указан
+				if (lineId in IDs) or idFileName == "":
+					if not (lineId in IDExclusionList):  # Если ID не находится в чёрном списке
+						if not (lineId in IDs):  # Если такого ID ещё не было, добавляем его в базу
+							IDs.append(lineId)
+						if not (lineId in rawOutput[fileName]):  # Если такого ID ещё не было, добавляем его в базу
+							rawOutput[fileName][lineId] = ID()
+						# Заносим значения из файла в объект
+						rawOutput[fileName][lineId]["counts"] += 1
+						rawOutput[fileName][lineId]["seq_length_summ"] += len(line[positions["Sequence"]])
+						rawOutput[fileName][lineId]["Sc_summ"] += float(line[positions["Sc"]])
+						rawOutput[fileName][lineId]["Psignal_summ"] += float(line[positions["Precursor Signal"]])
+						rawOutput[fileName][lineId]["Unused"] = float(line[positions["Unused"]])
+				if not (lineId in IDExclusionList):
+					if not (lineId in seqDB):  # Тест на присутствие id в Database file
+						print("ERROR:Incomplete database!")
+						input("File: " + fileName + "\nId: " + lineId)
+						exit()
+
+					rawOutput[fileName]["Sc_file_summ"] += float(line[positions["Sc"]])
+					rawOutput[fileName]["Psignal_file_summ"] += float(line[positions["Precursor Signal"]])
+
+	for name in rawOutput:
+		rawOutput[name]["sc_norm_summ"] = 0
+		rawOutput[name]["psignal_norm_summ"] = 0
+	#  Подсчёт отношений Psignal_summ и Sc_summ к длине последовательности из Database file для каждого ID
+	for id in IDs:
+		for name in rawOutput:
+			if id in rawOutput[name]:
+				try:
+					rawOutput[name][id]["Sc_norm"] = rawOutput[name][id]["Sc_summ"] / seqDB[id]
+					rawOutput[name][id]["Psignal_norm"] = rawOutput[name][id]["Psignal_summ"] / seqDB[id]
+				except Exception as e:
+					print(name, id, sep = "\n")
+					print(seqDB[id], end="\n\n")
+					raise e
+				rawOutput[name]["sc_norm_summ"] += rawOutput[name][id]["Sc_norm"]
+				rawOutput[name]["psignal_norm_summ"] += rawOutput[name][id]["Psignal_norm"]
+	return IDs, rawOutput
+
+# Конвертация в trueOutput (первичным параметром становится id) и удаление из вывода id, не соответствующих условию (группы)
+def rawOutputToTrueOutput(IDs, seqDB, rawOutput, shortFileNames, maxGroupAbsence, minGroupsWithId):
+	groupCount = 1
+	for i in range(1, len(shortFileNames)):
+		if shortFileNames[i].split('.')[0] != shortFileNames[i - 1].split('.')[0]:
+			groupCount += 1
+		i += 1
+
+	# Находим те элементы, которые отсутствуют слишком много раз
+	if (maxGroupAbsence != ""):
+		for id in IDs:
+			i = 0
+			groupsWithIdNum = groupCount
+			while (i < len(shortFileNames)):
+				j = i
+				groupAbsence = 0
+				while (j < len(shortFileNames)):
+					if shortFileNames[j].split(".")[0] != shortFileNames[i].split(".")[0]:
+						break
+					if not (id in rawOutput[shortFileNames[j]]):
+						groupAbsence+=1
+					j = j + 1
+				if groupAbsence > int(maxGroupAbsence):
+					groupsWithIdNum -= 1
+				i = j
+			if (groupsWithIdNum < minGroupsWithId):
+				for name in rawOutput:
+					if id in rawOutput[name]:
+						rawOutput[name].pop(id)
+
+	# Конвертируем в trueOutput
+	trueOutput = {}
+	for id in IDs:
+		for name in rawOutput:
+			if id in rawOutput[name]:
+				if not id in trueOutput:
+					trueOutput[id] = []
+				rawOutput[name][id]["Sc_norm"] = rawOutput[name][id]["Sc_norm"] / rawOutput[name]["sc_norm_summ"]
+				rawOutput[name][id]["Psignal_norm"] = rawOutput[name][id]["Psignal_norm"] / rawOutput[name]["psignal_norm_summ"]
+				rawOutput[name][id]["SP_2"] = (rawOutput[name][id]["Sc_norm"] + rawOutput[name][id]["Psignal_norm"]) / 2
+				rawOutput[name][id]["seq_length"] = seqDB[id]
+				trueOutput[id].append({"fileName": name, "params": rawOutput[name][id]})
+	return trueOutput, rawOutput
+
+def outputResultsToFiles(rawOutput, trueOutput, seqDB, seqDescDB, shortFileNames):
+	# Создание файлов по параметрам (1 файл - 1 параметр)
+	outputFiles = {}
+	if not exists("Output"):
+		mkdir("Output")
+	for param in params:
+		outputFiles[param] = (param + ".txt").replace(" ", "_")
+	for param in params:
+		outputFile = open(OUTPUTPATH + outputFiles[param], "w")
+		lines = [[id] for id in trueOutput]
+		if param == "Sc_summ":
+			lines.insert(0, ["ID", "Length", *shortFileNames])
+		else:
+			lines.insert(0, ["ID", *shortFileNames])
+		for line in lines[1:]:
+			if param == "Sc_summ":
+				line.append(str(seqDB[line[0]]))
+			for fileName in shortFileNames:
+				if line[0] in rawOutput[fileName]:
+					line.append(str(rawOutput[fileName][line[0]][param]))
+				else:
+					line.append("")
+
+		outputFile.write("\n".join(["\t".join(line) for line in lines]))
+		outputFile.close()
+	# Создание файла с «общим выводом» (со всеми параметрами)
+	with open(OUTPUTPATH + "output.txt", "w") as outputFile:
+		outputFile.write("ID\tFileName\t" + "\t".join(outParams) + "\n")
+		for key in trueOutput:
+			for fileName in shortFileNames:
+				for obj in trueOutput[key]:
+					if obj["fileName"] != fileName:
+						continue
+					outputFile.write( ("{}\t{}"+ "\t{}" * len(outParams) + "\n").format(
+							key, obj["fileName"], *[obj["params"][val] for val in outParams]
+					))
+					break
+		outputFile.close()
+
+	with open(OUTPUTPATH + "description.txt", 'w') as descFile:
+		descFile.write("ID\tDescription\n")
+		for key in trueOutput:
+			descFile.write(str(key) + "\t" + str(seqDescDB[key]) + "\n")
+		descFile.close()
+
+
+def main():
+	# Получение основных значений из пользовательского ввода
+	idFileName, IDs, IDExclusionList, seqDB, seqDescDB, unusedOp, unused, contribOp, contrib, confOp, conf, minGroupsWithId, maxGroupAbsence = \
+	inputParams(input("Id file name: "),
+								input("ID exclusion file name: "),
+								input("Database file name: "),
+								input("Unused: "),
+								input("Contribution: "),
+								input("Confidence: "),
+								input("Min groups with ID: "),
+								input("Max missing values per group: "))
+	#inputParams("", "IDexcl.txt", "astru4.fasta", ">=1", "", ">=95", "1", "3")
+
+	# Считывание и занесение в оперативную память информации из папки INPUTPATH
+	fileNames, db, shortFileNames, rawOutput = readInputFiles()
+	# Структурирование данных из файлов, загруженных в оперативную память. Первиный параметр - имя файла
+	IDs, rawOutput = getInfoFromFiles(idFileName, IDs, IDExclusionList, seqDB, db, rawOutput, confOp, conf, contribOp, contrib, unusedOp, unused)
+	trueOutput, rawOutput = rawOutputToTrueOutput(IDs, seqDB, rawOutput, shortFileNames, maxGroupAbsence, minGroupsWithId)
+	outputResultsToFiles(rawOutput, trueOutput, seqDB, seqDescDB, shortFileNames)
+
+try:
+	main()
+except Exception as e:
+	input(str(e))
+	raise(e)
---
+++ PeptideSummaryAnalyzer_8.py
@@ -0,0 +1,453 @@
+from typing import List, Dict, Union
+from os import listdir
+
+INPUTPATH = "./Input"
+
+
+# Класс для хранения последовательностей
+class Sequence():
+    len: int
+    desc: str
+    seq: str
+
+    def __init__(self) -> None:
+        self.len = 0
+        self.desc = ""
+        self.seq = ""
+
+
+# Класс для хранения параметров фильтра
+class Comparable():
+    op: str
+    val: str
+    __float: bool
+
+    def __init__(self, op: str="", unused: str="") -> None:
+        self.op: str = op
+        self.val: str = unused
+        self.__float: bool = False
+
+    @property
+    def val(self):
+        return self.__val
+
+    @val.setter
+    def val(self, value):
+        self.__val = value
+        if IsFloat(value):
+            self.__float = True
+        else:
+            self.__float = False
+
+    def compare(self, value, filename: str) -> bool:
+        if self.__float:
+            return eval("{value}{op}{comparable}".format(
+                value=value,
+                op=self.op,
+                comparable=self.val))
+        else:
+            if len(self.val) and filename in self.val:
+                return eval("{value}{op}{comparable}".format(
+                    value=value,
+                    op=self.op,
+                    comparable=self.val[filename]))
+            else:
+                return True
+
+
+# Может ли переменная быть приведена к типу float
+def IsFloat(var: str) -> bool:
+    try:
+        float(var)
+        return True
+    except (ValueError, TypeError):
+        return False
+
+
+def ReadTable(tableFilename: str, sep='\t') -> Dict[str, List[str]]:
+    """ Считывание файла-таблицы в словарь
+
+    На вход принимается имя файла, из которого считывать таблицу.
+    Формат таблицы:
+        Заголовок 1\tЗаголовок 2\t...\tЗаголовок N
+        Значение 1.1\tЗначение 2.1\t...\tЗначение N.1
+        Значение 1.2\tЗначение 2.2\t...\tЗначение N.2
+        .............................................
+        Значение 1.n\tЗначение 2.n\t...\tЗначение N.n
+    Первая строка считается как заголовки столбцов.
+    На выходе получается словарь вида:
+    {
+        "column name 1": ["value1", "value2", ..., "valueN"],
+        "column name 2": ["value1", "value2", ..., "valueN"],
+        "column name n": ["value1", "value2", ..., "valueN"]
+    }
+    """
+
+    with open(tableFilename) as tempFile:
+        strings = tempFile.read().split('\n')
+        tempFile.close()
+        table: Dict[str, List[str]] = {}
+        columns = strings[0].split(sep)
+
+        for column in columns:
+            table[column] = []
+        for string in strings[1:]:
+            if len(string.strip()):
+                i = 0
+                for value in string.split(sep):
+                    table[columns[i]].append(value)
+                    i += 1
+
+        return table
+    return None
+
+
+def ReadPeptideSummary(inputDir: str) -> Dict:
+    """ Считывание всех PeptideSummary файлов в словарь """
+    peptideTables = {}
+    for filename in listdir(inputDir):
+        if "Peptide" in filename:
+            peptideTables[filename.split('_')[0]] = (
+                ReadTable(inputDir + '/' + filename))
+    return peptideTables
+
+
+def ReadSeqDB(seqDBFilename: str) -> dict:
+    """ Считывание последовательностей из файла
+
+    Считывание длин последовательностей из файла БД с последовательностями в
+    словарь массивов вида {"id"} = {"seqLen", "seq", "seqDesc}"""
+
+    with open(seqDBFilename) as seqDBFile:
+        strings = seqDBFile.read().split('\n')
+        seqDBFile.close()
+        seqDB = {}
+        i = 0
+        while(i < len(strings)):
+            if len(strings[i]):
+                if strings[i][0] == '>':
+                    seqID = strings[i].split(' ')[0][1:]
+                    seqDB[seqID] = Sequence()
+                    if len(strings[i].split(' ')) > 1:
+                        seqDB[seqID].desc = strings[i].split(' ')[1]
+                        for word in strings[i].split(' ')[2:]:
+                            if word.startswith("OS="):
+                                break
+                            seqDB[seqID].desc += ' ' + word
+                    i += 1
+                    while ((i < len(strings))
+                           and (not len(strings[i]) or strings[i][0] != '>')):
+                        seqDB[seqID].seq += ''.join(
+                            [ch for ch in strings[i]
+                             if ch in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"])
+                        i += 1
+                    i -= 1
+                    seqDB[seqID].len = len(seqDB[seqID].seq)
+            i += 1
+
+        for seqID in seqDB:
+            if not seqDB[seqID].len:
+                input("Error!\nLength of sequence with id " + seqID + " = 0")
+                raise(IndexError)
+        return seqDB
+    return None
+
+
+def GetParam(paramString: str) -> Comparable:
+    """ Получение param
+
+    Получение param, общего для всех фалов, либо для каждого своего, из строки.
+    Формат: [операция][[имя файла] или [число]]
+    Примеры:
+        >=param
+        < paramList.txt"""
+
+    paramString = paramString.strip()
+    param = Comparable(
+        op=''.join([ch for ch in paramString if ch in "!=<>"]))
+
+    paramString = paramString[len(param.op):].strip()
+    if IsFloat(paramString):
+        param.val = float(paramString)
+    elif len(param.op) and len(paramString):
+        with open(paramString) as paramStringFile:
+            strings = paramStringFile.read().replace(' ', '\t').split('\n')
+            paramStrings = {}
+            for string in strings:
+                string = string.strip()
+                if len(string):
+                    paramStrings[string.split('\t')[0]] = string.split('\t')[1]
+            param.val = paramStrings
+    return param
+
+
+def GetFileLines(filename: str) -> list:
+    """ Returns list of file strings without newline symbols """
+
+    try:
+        with open(filename) as tfile:
+            return tfile.read().split('\n')
+    except FileNotFoundError:
+        return None
+
+
+def RemoveRow(table: Dict[str, List[str]], rowNum: int) -> None:
+    """ Удаляет строку из таблицы
+
+    Удаляет строку из таблицы вида
+    {
+        "Заголовок 1": ["Значение 1", "Значение 2", ..., "Значение n1"],
+        "Заголовок 2": ["Значение 1", "Значение 2", ..., "Значение n1"],
+        ..............................................................
+        "Заголовок N1": ["Значение 1", "Значение 2", ..., "Значение n1"]
+    } """
+    columns = [column for column in table]
+    for column in columns:
+        del table[column][rowNum]
+
+
+def ApplyParamsFilter(unused: Comparable,
+                      contrib: Comparable,
+                      conf: Comparable,
+                      peptideTables: Dict[str, Dict[str, List[str]]]) -> dict:
+    """ Применяем фильтры, завязанные на параметры unused, contib, conf.
+
+    peptideTables - словарь вида
+    {
+        "1.1": {
+            "Заголовок 1": ["Значение 1", "Значение 2", ..., "Значение n1"],
+            "Заголовок 2": ["Значение 1", "Значение 2", ..., "Значение n1"],
+            ..............................................................
+            "Заголовок N1": ["Значение 1", "Значение 2", ..., "Значение n1"]
+        },
+        "1.2": {
+            "Заголовок 1": ["Значение 1", "Значение 2", ..., "Значение n2"],
+            "Заголовок 2": ["Значение 1", "Значение 2", ..., "Значение n2"],
+            ..............................................................
+            "Заголовок N2": ["Значение 1", "Значение 2", ..., "Значение n2"]
+        },
+        ...,
+        "I-ый файл": {
+            "Заголовок 1": ["Значение 1", "Значение 2", ..., "Значение nI"],
+            "Заголовок 2": ["Значение 1", "Значение 2", ..., "Значение nI"],
+            ..............................................................
+            "Заголовок NI": ["Значение 1", "Значение 2", ..., "Значение nI"]
+        }
+    }
+
+    Возвращаем словарь с уже применёнными фильрами.
+    """
+
+    for tableNum in peptideTables:
+        curTable = peptideTables[tableNum]
+        curTableLen = len(curTable["Unused"])
+        i = 0
+
+        while i < curTableLen:
+            if not (unused.compare(curTable["Unused"][i], tableNum) and
+                    contrib.compare(curTable["Contrib"][i], tableNum) and
+                    conf.compare(curTable["Conf"][i], tableNum)):
+                RemoveRow(curTable, i)
+                i -= 1
+                curTableLen -= 1
+            i += 1
+
+    return peptideTables
+
+
+def ApplyDefaultConf(peptideTables: Dict[str, Dict[str, List[str]]]):
+    """ default-условие: Accession учитывается, если хотя бы одна
+    из строк с ним имеет Conf >= 99 или минимум две строки имеют
+    Conf >= 95"""
+
+    for tableNum in peptideTables:
+        curTable = peptideTables[tableNum]
+        curTableLen = len(curTable["Unused"])
+        # Заносим все Accession в список Accession для удаления
+        # В процессе чтения списка записи из него будут удаляться
+        blackListByConfCondition: Dict[str, int] = {}
+        for accession in curTable["Accessions"]:
+            if accession not in blackListByConfCondition:
+                blackListByConfCondition[
+                    accession.split(';')[0]] = 0
+
+        i = 0
+        while i < curTableLen:
+            curAccession = curTable["Accessions"][i].split(';')[0]
+            if curAccession in blackListByConfCondition:
+                if float(curTable["Conf"][i]) >= 99.0:
+                    del blackListByConfCondition[curAccession]
+                elif float(curTable["Conf"][i]) >= 95.0:
+                    blackListByConfCondition[curAccession] += 1
+                    if blackListByConfCondition[curAccession] > 1:
+                        del blackListByConfCondition[curAccession]
+        i = 0
+        while i < curTableLen:
+            if(curTable["Accessions"][i].split(';')[0] in
+               blackListByConfCondition):
+                RemoveRow(curTable, i)
+                curTableLen -= 1
+                i -= 1
+            i += 1
+    return peptideTables
+
+
+# TODO: Сделать применение белого/чёрного списков
+def ApplyBlackList(peptideTables: Dict[str, Dict[str, List[str]]],
+                   blackList: List[str]):
+    """ Удаляем из всех таблиц все id, находящиеся в чёрном списке """
+
+    for tableNum in peptideTables:
+        curTable = peptideTables[tableNum]
+        curTableLen = len(curTable["Accessions"])
+        i = 0
+        while i < curTableLen:
+            if curTable["Accessions"][i].split(';')[0] in blackList:
+                RemoveRow(curTable, i)
+                i -= 1
+                curTableLen -= 1
+            i += 1
+
+
+def ApplyWhiteList(peptideTables: Dict[str, Dict[str, List[str]]],
+                   whiteList: List[str]):
+    """ Удаляем из всех таблиц все id, отсутствующие в белом списке """
+
+    for tableNum in peptideTables:
+        curTable = peptideTables[tableNum]
+        curTableLen = len(curTable["Accessions"])
+        i = 0
+        while i < curTableLen:
+            if curTable["Accessions"][i].split(';')[0] not in whiteList:
+                RemoveRow(curTable, i)
+                i -= 1
+                curTableLen -= 1
+            i += 1
+
+
+# TODO: сделать подсчёт Sc, Seq и Psignal сумм
+def GetScPsigFileSumm(peptideTables: Dict[str, Dict[str, List[str]]]) -> Dict:
+    """ Получаем суммы параметров Sc, Sequence, PrecursorSignal по файлам
+
+    peptideTables - словарь вида
+    {
+        "1.1": {
+            "Заголовок 1": ["Значение 1", "Значение 2", ..., "Значение n1"],
+            "Заголовок 2": ["Значение 1", "Значение 2", ..., "Значение n1"],
+            ..............................................................
+            "Заголовок N1": ["Значение 1", "Значение 2", ..., "Значение n1"]
+        },
+        "1.2": {
+            "Заголовок 1": ["Значение 1", "Значение 2", ..., "Значение n2"],
+            "Заголовок 2": ["Значение 1", "Значение 2", ..., "Значение n2"],
+            ..............................................................
+            "Заголовок N2": ["Значение 1", "Значение 2", ..., "Значение n2"]
+        },
+        ...,
+        "I-ый файл": {
+            "Заголовок 1": ["Значение 1", "Значение 2", ..., "Значение nI"],
+            "Заголовок 2": ["Значение 1", "Значение 2", ..., "Значение nI"],
+            ..............................................................
+            "Заголовок NI": ["Значение 1", "Значение 2", ..., "Значение nI"]
+        }
+    }
+
+    Возвращаем словарь с суммами вида
+    {
+        "1.1": {
+            "Sc": S1,
+            "pSignal": P1
+        },
+        "1.2": {
+            "Sc": S2,
+            "pSignal": P2
+        },
+        ...,
+        "N-ый файл": {
+            "Sc": SN,
+            "pSignal": PN
+        }
+    }
+    """
+
+    fileSumms: Dict[str, Dict[str, Union[float, int]]] = {}
+
+    for tableNum in peptideTables:
+        curTable = peptideTables[tableNum]
+        fileSumms[tableNum] = {}
+        curSumm = fileSumms[tableNum]
+        curSumm["Sc"] = 0
+        curSumm["pSignal"] = 0
+
+        i = 0
+        while i < len(curTable["Sc"]):
+            curSumm["Sc"] += float(curTable["Sc"][i])
+            curSumm["pSignal"] += float(curTable["PrecursorSignal"][i])
+    return fileSumms
+
+
+def main():
+    peptideTables = ReadPeptideSummary(INPUTPATH)
+    seqDB = ReadSeqDB("EFRA_cont.fasta")
+    unused = GetParam(">=unusedList.txt")
+    contrib = GetParam(">=1")
+    conf = ">=50"
+    whiteList = GetFileLines("")
+    blackList = GetFileLines("IDexcl.txt")
+
+    # Create copy of peptideTables and store into temp var
+    temp = {}
+    for table in peptideTables:
+        temp[table] = {}
+        for column in peptideTables[table]:
+            temp[table][column] = peptideTables[table][column].copy()
+
+    if conf.strip() is "default":
+        ApplyDefaultConf(peptideTables)
+        ApplyParamsFilter(unused, contrib, GetParam(""), peptideTables)
+    else:
+        ApplyParamsFilter(unused, contrib, GetParam(conf), peptideTables)
+
+    from dictdiffer import diff  # type: ignore
+    print(*list(diff(temp, peptideTables)), sep='\n')
+    # minGroupsWithId = 20
+    # maxGroupAbsence = 5
+
+    print("whiteList:\n  {}".format(whiteList))
+    print("blackList:\n" + ("  {}\n" * len(blackList)).format(*blackList))
+
+    # Create copy of peptideTables and store into temp var
+    temp = {}
+    for table in peptideTables:
+        temp[table] = {}
+        for column in peptideTables[table]:
+            temp[table][column] = peptideTables[table][column].copy()
+
+    ApplyBlackList(peptideTables, blackList)
+    fileSumms = GetScPsigFileSumm()
+
+    print(*list(diff(temp, peptideTables)), sep='\n')
+
+    for table in peptideTables:
+        """
+        for column in peptideTables[table]:
+            print("Column {column} of table {table}:\n{column_data}".format(
+                column_data = "\t".join(peptideTables[table][column]),
+                column = column,
+                table = table))
+        print('\n')
+        """
+        pass
+    for seqID in seqDB:
+        """
+        print("Sequence with id {}:\n  len: {}\n  desc: {}\n  seq: {}".format(
+            seqID,
+            seqDB[seqID].len,
+            seqDB[seqID].desc,
+            seqDB[seqID].seq))
+        """
+        pass
+
+
+if __name__ == "__main__":
+    main()
