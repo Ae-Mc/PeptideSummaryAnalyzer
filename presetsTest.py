9256fc422d058191c0156134af7076d754875dcd
Ae-Mc <ae_mc@mail.ru>
2021-06-04 17:49:15 +0300

    Read new params from presets, reformat with black

--- presetsTest.py
+++ presetsTest.py
@@ -22,13 +22,15 @@ class Preset:
         settings: настройки пресета
         errorCode: код ошибки, если она была
     """
+
     folder: str
     presetOutputDir: str
     settings: Input
     errorCode: int = 0
 
     def __init__(self, folder: str):
-        """Args:
+        """
+        Args:
             folder: папка с пресетом
         """
         self.folder = folder
@@ -61,39 +63,47 @@ class Preset:
             try:
                 self.TestPresetFile(presetFileLines)
             except IOError as e:
-                print(f"Error reading preset {path.split(self.folder)[1]} "
-                      f"{e.args[0]}")
+                print(
+                    f"Error reading preset {path.split(self.folder)[1]} "
+                    f"{e.args[0]}"
+                )
                 self.errorCode = 1
                 return
             presetFileValues = [
-                line.split(':')[1].strip() for line in presetFileLines
-                if len(line.split(':')) > 1]
+                line.split(":")[1].strip()
+                for line in presetFileLines
+                if len(line.split(":")) > 1
+            ]
 
         self.settings.blackList = None
         blackList = GetFileLines(path.join(self.folder, presetFileValues[1]))
         if len(presetFileValues[1].strip()):
-            self.settings.blackList = (None if blackList is None
-                                       else (presetFileValues[1], blackList))
-        self.settings.isProteinGroupFilter = presetFileValues[2].lower()
+            self.settings.blackList = (
+                None if blackList is None else (presetFileValues[1], blackList)
+            )
         self.settings.seqDB = SequenceDatabase.fromFile(
-            FindFastaFile(self.folder))
-        self.settings.proteinConfidence = presetFileValues[3]
-        self.settings.confPeptide = presetFileValues[4]
-        self.settings.minGroupsWithAccession = int(presetFileValues[5])
-        self.settings.maxGroupAbsence = int(presetFileValues[6])
+            FindFastaFile(self.folder)
+        )
+        self.settings.proteinConfidence = presetFileValues[2]
+        self.settings.confPeptide = presetFileValues[3]
+        self.settings.minGroupsWithAccession = int(presetFileValues[4])
+        self.settings.maxGroupAbsence = int(presetFileValues[5])
 
     def TestPresetFile(self, presetFileLines: List[str]) -> None:
         """Проверка того, правильно ли написан файл с настроками пресета"""
         neccessaryStringParts = [
-            ('ProteinPilot summary analyzer', 'Program name'),
-            ('#Protein filter-', 'Protein filter header'),
-            ('FDR', 'Global FDR value'),
+            ("ProteinPilot summary analyzer", "Program name"),
+            ("#Protein filter", "Protein filter header"),
+            ("FDR", "Global FDR value"),
             ("ID exclusion list", "ID blacklist"),
-            ("Protein group filter", "Protein group filter"),
             ("Peptide confidence", "Protein group filter peptide confidence"),
-            ('#Peptide filter-', 'Peptide filter header'),
+            (
+                "Protein grouping (conf)",
+                "Protein group filter peptide confidence",
+            ),
+            ("#Peptide filter", "Peptide filter header"),
             ("Peptide confidence", "Peptide filter peptide confidence"),
-            ('#Output filter-', 'Output filter header'),
+            ("#Output filter", "Output filter header"),
             ("Min groups", "Min groups"),
             ("Max missing values", "Max missing values per group"),
         ]
@@ -106,7 +116,8 @@ class Preset:
         if not path.exists(path.join(self.folder, "preset.txt")):
             self.errorCode = 1
             raise FileNotFoundError(
-                f"ERROR!!! preset.txt file not found in preset {self.folder}")
+                f"ERROR!!! preset.txt file not found in preset {self.folder}"
+            )
 
     def TestResult(self) -> None:
         """Проверка выходных файлов"""
@@ -114,7 +125,7 @@ class Preset:
             try:
                 self.TestOutputFile(filename)
             except FileNotFoundError:
-                print(f"Error! File \"{filename}\" not found in Output dir!")
+                print(f'Error! File "{filename}" not found in Output dir!')
                 self.errorCode = 1
 
     def TestOutputFile(self, filename: str) -> None:
@@ -123,18 +134,22 @@ class Preset:
         Args:
             filename: имя файла для проверки
         """
-        paths = (path.join(self.presetOutputDir, filename),
-                 path.join(self.settings.outputPath, filename))
+        paths = (
+            path.join(self.presetOutputDir, filename),
+            path.join(self.settings.outputPath, filename),
+        )
         with open(paths[0]) as file1:
             with open(paths[1]) as file2:
-                filesContent = (file1.read().strip().split("\n"),
-                                file2.read().strip().split("\n"))
-        if(len(filesContent[0]) != len(filesContent[1])):
+                filesContent = (
+                    file1.read().strip().split("\n"),
+                    file2.read().strip().split("\n"),
+                )
+        if len(filesContent[0]) != len(filesContent[1]):
             print(f"{paths[0]} != {paths[1]}")
             self.errorCode = 2
         else:
             for i in range(0, len(filesContent[0])):
-                if(filesContent[0][i] != filesContent[1][i]):
+                if filesContent[0][i] != filesContent[1][i]:
                     print(f"Line {i+1} is not equals in file {filename}")
                     self.errorCode = 3
                     break
@@ -146,7 +161,7 @@ class Preset:
                 remove(path.join(self.settings.outputPath, filename))
 
 
-def GetPresetsFolders(presetFolder: str) -> List[str]:
+def GetPresetsFolders() -> List[str]:
     """Получает список папок с пресетами
 
     Args:
@@ -173,7 +188,7 @@ def main():
         if preset.errorCode:
             errorCode = preset.errorCode
     else:
-        presetsFolders = GetPresetsFolders(presetsFolder)
+        presetsFolders = GetPresetsFolders()
         for folder in presetsFolders:
             preset = Preset(folder)
             preset.ReadSettings()
