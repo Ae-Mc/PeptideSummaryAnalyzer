74355f6df5902f2082980bfb94fa0b44e8a00211
Ae-Mc <ae_mc@mail.ru>
2021-09-14 23:51:55 +0300

    Rewrite Comparable class to satisfy new sql requests

--- Classes/Comparable.py
+++ Classes/Comparable.py
@@ -1,4 +1,5 @@
-from typing import Optional, Union, Dict
+from re import match, sub
+from typing import Optional
 
 
 class Comparable:
@@ -8,21 +9,13 @@ class Comparable:
 
     Attributes:
         op: операция сравнения
-        val: значение, с которым будет производится сравнение, либо словарь
-            вида: {
-                "имя файла": значение
-            }.
-            Также может быть равно None, в этом случае все операции сравнения
-            вернут истину
-    """
+        val: значение, с которым будет производится сравнение"""
 
     op: Optional[str]
-    __val: Optional[Union[str, Dict[str, str]]]
-    __isFileToCompare: bool
+    val: Optional[str]
 
     def __init__(self, op: str = None, val: str = None) -> None:
-        self.__isFileToCompare: bool = True
-        if val is not None:
+        if not (val is None or op is None):
             self.op = op
             self.val = val
         elif op is not None:
@@ -31,88 +24,26 @@ class Comparable:
             self.op = None
             self.val = None
 
-    @property
-    def val(self):
-        return self.__val
-
-    @val.setter
-    def val(self, value):
-        try:
-            float(value)
-            self.__val = value
-            self.__isFileToCompare = False
-        except (TypeError, ValueError):
-            if value is None or (
-                isinstance(value, str) and len(value.strip()) == 0
-            ):
-                self.__val = value
-            elif isinstance(value, dict):
-                self.__val = value
-                self.__isFileToCompare = True
-            else:
-                self._LoadFromFile(value)
-                self.__isFileToCompare = True
-
     def GetComparable(self, paramString: str):
         """Получение параметра
 
-        Получение параметра, общего для всех фалов, либо для каждого своего, из
-        строки.
-        Формат: [операция][[имя файла] или [число]]
+        Формат: [операция][число]
 
         Args:
             paramString: строка, которая будет ковертирована в параметр
 
         Примеры:
-            >=99
-            < paramList.txt"""
+            >=99"""
 
-        paramString = paramString.strip()
-        self.op = "".join([ch for ch in paramString if ch in "!=<>"])
-
-        paramValue = paramString[len(self.op) :].strip()
-        if paramValue.strip() == "":
+        x = match(r"^(([<>]=?)|=) *([-\d]+)$", sub("$==", "=", paramString.strip()))
+        if x:
+            self.op = x.group(1)
+            self.val = x.group(3)
+        elif paramString.strip() == "":
+            self.op = None
             self.val = None
         else:
-            self.val = paramValue
-
-    def _LoadFromFile(self, filename: str):
-        """Получение значений параметра из файла
-
-        Args:
-            filename: имя файла, из которого будут считываться значения
-        """
-        with open(filename) as paramStringFile:
-            strings = paramStringFile.read().replace(" ", "\t").split("\n")
-            paramStrings = {}
-            for string in strings:
-                string = string.strip()
-                if len(string):
-                    paramStrings[string.split("\t")[0]] = string.split("\t")[1]
-            self.val = paramStrings
-
-    def compare(
-        self, value: Union[str, float, int], filename: str = None
-    ) -> bool:
-        """Сравнивает значение с параметром
-
-        Args:
-            value: значение, которое будет сравниваться
-            filename: имя файла, если параметр был считан из файла
-        Returns:
-            Результат сравнения значения с параметром
-        """
-        if self.val is None:
-            return True
-        elif isinstance(self.val, str) or self.__isFileToCompare:
-            return eval(f"{value}{self.op}{self.val}")
-        elif (
-            self.val is not None
-            and filename is not None
-            and filename in self.val
-        ):
-            return eval(f"{value}{self.op}{self.val[filename]}")
-        return True
+            raise ValueError("Неверный формат параметра для сравнения")
 
     def __str__(self):
         return self.__repr__()
