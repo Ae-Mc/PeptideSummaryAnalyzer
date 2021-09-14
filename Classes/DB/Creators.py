119d4d6b5e3c2eb1842600c6195999d11b72d5a4
Ae-Mc <ae_mc@mail.ru>
2021-09-14 23:50:33 +0300

    Add to tables and one function creation

--- Classes/DB/Creators.py
+++ Classes/DB/Creators.py
@@ -1,8 +1,12 @@
 from sqlite3.dbapi2 import Cursor
 
 
+def IsReversed(accession: str) -> bool:
+    return accession.startswith("RRRRR")
+
+
 class Creators:
-    """Отвечает за создание таблиц
+    """Отвечает за создание таблиц и дополнительных функций
 
     Attributes:
         cursor: cursor, через который создаются таблицы"""
@@ -12,12 +16,18 @@ class Creators:
     def __init__(self, cursor: Cursor) -> None:
         self.cursor = cursor
 
+    def createAllFunctions(self) -> None:
+        self.cursor.connection.create_function("IS_REVERSED", 1, IsReversed)
+
     def createAllTables(self):
         self.createSequenceTable()
         self.createExclusionTable()
         self.createPeptideRowTable()
         self.createPeptideAccessionTable()
         self.createPeptideTable()
+        self.createRepresentativeTable()
+        self.createGroupTable()
+        self.createPeptideWithSum()
 
     def createSequenceTable(self) -> None:
         self.cursor.execute(
@@ -71,5 +81,39 @@ class Creators:
                 score FLOAT NOT NULL,
                 peptide_intensity FLOAT NOT NULL,
                 sequence TEXT NOT NULL
+            );"""
+        )
+
+    def createRepresentativeTable(self) -> None:
+        self.cursor.execute(
+            """CREATE TABLE representative (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                table_number TEXT NOT NULL,
+                accession TEXT NOT NULL
+            );"""
+        )
+
+    def createGroupTable(self) -> None:
+        self.cursor.execute(
+            """CREATE TABLE accession_group (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                representative_id INTEGER NOT NULL,
+                accession TEXT NOT NULL,
+                count_in_table INTEGER NOT NULL
+            );"""
+        )
+
+    def createPeptideWithSum(self) -> None:
+        self.cursor.execute(
+            """CREATE TABLE peptide_with_sum (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                table_number TEXT NOT NULL,
+                accession TEXT NOT NULL,
+                sc_sum FLOAT NOT NULL,
+                peptide_intensity_sum FLOAT NOT NULL,
+                sc_norm FLOAT NOT NULL,
+                peptide_intensity_norm FLOAT NOT NULL,
+                sc_norm_to_file_norm_ratio FLOAT NOT NULL,
+                peptide_intensity_norm_to_file_norm_ratio FLOAT NOT NULL
             );"""
         )
