b44852a9983563659ee5614ae8bb0fdae89d9ef7
Ae-Mc <ae_mc@mail.ru>
2021-09-27 23:56:04 +0300

    Add prettyPrintRepresentatives method to DB

--- Classes/DB/db.py
+++ Classes/DB/db.py
@@ -1,10 +1,12 @@
-from Classes.DB.ProteinGrouping import ProteinGrouping
-from Classes.DB.Functions import Functions
-from Classes.DB.Fillers import Fillers
-from Classes.DB.FDR import FDR
-from typing import Any, Iterable, List, Tuple
-from Classes.DB.Creators import Creators
 from sqlite3.dbapi2 import Connection, Cursor, connect
+from sys import stdout
+from typing import Any, Dict, Iterable, List, TextIO, Tuple
+
+from Classes.DB.Creators import Creators
+from Classes.DB.FDR import FDR
+from Classes.DB.Fillers import Fillers
+from Classes.DB.Functions import Functions
+from Classes.DB.ProteinGrouping import ProteinGrouping
 
 
 class DB:
@@ -55,6 +57,54 @@ class DB:
         result = cursor.fetchall()
         result.insert(0, tuple(map(lambda x: x[0], cursor.description)))
         return result
+
+    def prettyPrintRepresentatives(self, file: TextIO = stdout):
+        rows = self.execute(
+            """SELECT repr.*, acc_g.accession, acc_g.count_in_table
+            FROM representative repr
+                INNER JOIN accession_group acc_g ON repr.id = acc_g.representative_id
+            WHERE (
+                SELECT COUNT(*)
+                FROM accession_group acc_g
+                WHERE acc_g.representative_id = repr.id
+            ) > 1;
+            """
+        ).fetchall()
+        tableNumbers = tuple(
+            map(
+                lambda x: x[0],
+                self.execute(
+                    """SELECT DISTINCT table_number
+            FROM representative repr
+            WHERE (
+                SELECT COUNT(*)
+                FROM accession_group acc_g
+                WHERE acc_g.representative_id = repr.id
+            ) > 1
+            ORDER BY CAST(table_number AS DECIMAL);"""
+                ).fetchall(),
+            )
+        )
+        print("Repr\tAcc\t", "\t".join(tableNumbers), file=file)
+        groups: Dict[str, Dict[str, List[str]]] = {}
+        for row in rows:
+            if row[3] not in groups:
+                groups[row[3]] = {}
+            if row[4] not in groups[row[3]]:
+                groups[row[3]][row[4]] = ["0" for _ in tableNumbers]
+            groups[row[3]][row[4]][tableNumbers.index(row[2])] = str(row[5])
+
+        for representative, accessions in groups.items():
+            print(
+                representative,
+                end="\t\t" + "\t".join(accessions[representative]) + "\n",
+                file=file,
+            )
+
+            for accession, counts in filter(
+                lambda p: p[0] != representative, accessions.items()
+            ):
+                print("\t".join(["", accession, *counts]), file=file)
 
     def close(self) -> None:
         self.connection.close()
