d8cc1e1315c8dec05b8a94b0cc4ea83133e20ea3
Ae-Mc <ae_mc@mail.ru>
2021-10-12 23:09:34 +0300

    Add seq_length_sum parameter to peptide_with_sum
    Add --sql to all SQL strings to enable syntax highlighting

--- Classes/DB/Creators.py
+++ Classes/DB/Creators.py
@@ -39,7 +39,8 @@ class Creators:
 
     def createSequenceTable(self) -> None:
         self.cursor.execute(
-            """CREATE TABLE sequence (
+            """--sql
+            CREATE TABLE sequence (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 accession TEXT NOT NULL UNIQUE,
                 description TEXT,
@@ -49,7 +50,8 @@ class Creators:
 
     def createExclusionTable(self) -> None:
         self.cursor.execute(
-            """CREATE TABLE exclusion (
+            """--sql
+            CREATE TABLE exclusion (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 accession TEXT NOT NULL UNIQUE
             );"""
@@ -57,7 +59,8 @@ class Creators:
 
     def createPeptideRowTable(self) -> None:
         self.cursor.execute(
-            """CREATE TABLE peptide_row (
+            """--sql
+            CREATE TABLE peptide_row (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 table_number TEXT NOT NULL,
                 confidence FLOAT NOT NULL,
@@ -69,7 +72,8 @@ class Creators:
 
     def createPeptideAccessionTable(self) -> None:
         self.cursor.execute(
-            """CREATE TABLE peptide_accession (
+            """--sql
+            CREATE TABLE peptide_accession (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 row_id INTEGER NOT NULL,
                 accession TEXT NOT NULL,
@@ -79,15 +83,17 @@ class Creators:
 
     def createJointPeptideTableView(self) -> None:
         self.cursor.execute(
-            """CREATE VIEW peptide_joint AS
+            """--sql
+            CREATE VIEW peptide_joint AS
             SELECT row.*, p_acc.accession
             FROM peptide_row row INNER JOIN peptide_accession p_acc
-                ON row.id = p_acc.row_id"""
+                ON row.id = p_acc.row_id;"""
         )
 
     def createPeptideTable(self) -> None:
         self.cursor.execute(
-            """CREATE TABLE peptide (
+            """--sql
+            CREATE TABLE peptide (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 table_number TEXT NOT NULL,
                 accession TEXT NOT NULL,
@@ -100,7 +106,8 @@ class Creators:
 
     def createRepresentativeTable(self) -> None:
         self.cursor.execute(
-            """CREATE TABLE representative (
+            """--sql
+            CREATE TABLE representative (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 row_id INTEGER NOT NULL,
                 table_number TEXT NOT NULL,
@@ -111,7 +118,8 @@ class Creators:
 
     def createGroupTable(self) -> None:
         self.cursor.execute(
-            """CREATE TABLE accession_group (
+            """--sql
+            CREATE TABLE accession_group (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 representative_id INTEGER NOT NULL,
                 accession TEXT NOT NULL,
@@ -121,11 +129,13 @@ class Creators:
 
     def createPeptideWithSum(self) -> None:
         self.cursor.execute(
-            """CREATE TABLE peptide_with_sum (
+            """--sql
+            CREATE TABLE peptide_with_sum (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 table_number TEXT NOT NULL,
                 accession TEXT NOT NULL,
                 count INT NOT NULL,
+                seq_length_sum INT NOT NULL,
                 sc_sum FLOAT NOT NULL,
                 peptide_intensity_sum FLOAT NOT NULL,
                 sc_norm FLOAT NOT NULL,
