86a2d2898420eabd030963eb2fdc5b7df948faad
Ae-Mc <ae_mc@mail.ru>
2021-10-13 16:11:39 +0300

    Strip all .fasta file lines

--- Classes/SequenceDatabase.py
+++ Classes/SequenceDatabase.py
@@ -20,24 +20,26 @@ class SequenceDatabase(dict):
         """
 
         with open(seqDBFilename) as seqDBFile:
-            strings = seqDBFile.read().strip().split("\n")
+            strings = list(
+                map(lambda x: x.strip(), seqDBFile.read().strip().split("\n"))
+            )
             seqDBFile.close()
             seqDB = SequenceDatabase()
             i = 0
             while i < len(strings):
-                if len(strings[i].strip()) > 0:
-                    if strings[i].strip()[0] == ">":
-                        seqID = strings[i].strip().split(" ")[0][1:]
+                if len(strings[i]) > 0:
+                    if strings[i][0] == ">":
+                        seqID = strings[i].split(" ")[0][1:]
                         seqDB[seqID] = Sequence(accession=seqID)
-                        if len(strings[i].strip().split(" ")) > 1:
-                            seqDB[seqID].desc = strings[i].strip().split(" ")[1]
-                            for word in strings[i].strip().split(" ")[2:]:
+                        if len(strings[i].split(" ")) > 1:
+                            seqDB[seqID].desc = strings[i].split(" ")[1]
+                            for word in strings[i].split(" ")[2:]:
                                 if word.startswith("OS="):
                                     break
                                 seqDB[seqID].desc += " " + word
                         i += 1
-                        while (i < len(strings)) and strings[i].strip()[0] != ">":
-                            seqDB[seqID].rawSeq += strings[i].strip()
+                        while (i < len(strings)) and strings[i][0] != ">":
+                            seqDB[seqID].rawSeq += strings[i]
                             i += 1
                         i -= 1
                         if not seqDB[seqID].len:
