2807ec47b614e573e3ff090d07383632bb0d0609
Ae-Mc <ae_mc@mail.ru>
2021-10-27 18:20:01 +0300

    Update autozip.py to follow new folders tree

---
+++ autozip.py
@@ -0,0 +1,24 @@
+import zipfile
+import os
+
+
+def archive_folder(archive: zipfile.ZipFile, folder_path: str) -> None:
+    for element in os.listdir(folder_path):
+        element_path = os.path.join(folder_path, element)
+        if not (element.startswith("__") or element.startswith(".")):
+            if os.path.isfile(element_path):
+                archive.write(element_path)
+            elif os.path.isdir(element_path):
+                archive_folder(archive, element_path)
+
+
+def main():
+    with zipfile.ZipFile(
+        "PeptideSummaryAnalyzer.zip", "w", zipfile.ZIP_STORED
+    ) as archive:
+        archive.write("sql.py")
+        archive_folder(archive, "classes")
+
+
+if __name__ == "__main__":
+    main()
